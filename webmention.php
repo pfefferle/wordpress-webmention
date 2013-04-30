f7408a39d8769034b0b7d060cb9d26660b8eee1b
pfefferle <pfefferle@gmail.com>
2013-04-30 18:22:45 +0200

    tabs to spaces

--- webmention.php
+++ webmention.php
@@ -77,56 +77,56 @@ add_action( 'pre_ping', 'webmention_ping', 10, 3 );
  * @return bool|string False on failure, string containing URI on success.
  */
 function discover_webmention_server_uri( $url ) {
-	$webmention_str_dquote = 'rel="http://webmention.org/"';
-	$webmention_str_squote = 'rel=\'http:\/\/webmention.org\/\'';
+  $webmention_str_dquote = 'rel="http://webmention.org/"';
+  $webmention_str_squote = 'rel=\'http:\/\/webmention.org\/\'';
 
-	/** @todo Should use Filter Extension or custom preg_match instead. */
-	$parsed_url = parse_url($url);
+  /** @todo Should use Filter Extension or custom preg_match instead. */
+  $parsed_url = parse_url($url);
 
-	if ( ! isset( $parsed_url['host'] ) ) // Not an URL. This should never happen.
-		return false;
+  if ( ! isset( $parsed_url['host'] ) ) // Not an URL. This should never happen.
+    return false;
 
-	//Do not search for a webmention server on our own uploads
-	$uploads_dir = wp_upload_dir();
-	if ( 0 === strpos($url, $uploads_dir['baseurl']) )
-		return false;
+  //Do not search for a webmention server on our own uploads
+  $uploads_dir = wp_upload_dir();
+  if ( 0 === strpos($url, $uploads_dir['baseurl']) )
+    return false;
 
-	$response = wp_remote_head( $url, array( 'timeout' => 2, 'httpversion' => '1.0' ) );
+  $response = wp_remote_head( $url, array( 'timeout' => 2, 'httpversion' => '1.0' ) );
 
-	if ( is_wp_error( $response ) )
-		return false;
+  if ( is_wp_error( $response ) )
+    return false;
 
-	if ( wp_remote_retrieve_header( $response, 'http://webmention.org/' ) )
-		return wp_remote_retrieve_header( $response, 'http://webmention.org/' );
+  if ( wp_remote_retrieve_header( $response, 'http://webmention.org/' ) )
+    return wp_remote_retrieve_header( $response, 'http://webmention.org/' );
 
-	// Not an (x)html, sgml, or xml page, no use going further.
-	if ( preg_match('#(image|audio|video|model)/#is', wp_remote_retrieve_header( $response, 'content-type' )) )
-		return false;
+  // Not an (x)html, sgml, or xml page, no use going further.
+  if ( preg_match('#(image|audio|video|model)/#is', wp_remote_retrieve_header( $response, 'content-type' )) )
+    return false;
 
-	// Now do a GET since we're going to look in the html headers (and we're sure its not a binary file)
-	$response = wp_remote_get( $url, array( 'timeout' => 2, 'httpversion' => '1.0' ) );
+  // Now do a GET since we're going to look in the html headers (and we're sure its not a binary file)
+  $response = wp_remote_get( $url, array( 'timeout' => 2, 'httpversion' => '1.0' ) );
 
-	if ( is_wp_error( $response ) )
-		return false;
+  if ( is_wp_error( $response ) )
+    return false;
 
-	$contents = wp_remote_retrieve_body( $response );
+  $contents = wp_remote_retrieve_body( $response );
 
-	$webmention_link_offset_dquote = strpos($contents, $webmention_str_dquote);
-	$webmention_link_offset_squote = strpos($contents, $webmention_str_squote);
-	if ( $webmention_link_offset_dquote || $webmention_link_offset_squote ) {
-		$quote = ($webmention_link_offset_dquote) ? '"' : '\'';
-		$webmention_link_offset = ($quote=='"') ? $webmention_link_offset_dquote : $webmention_link_offset_squote;
-		$webmention_href_pos = @strpos($contents, 'href=', $webmention_link_offset);
-		$webmention_href_start = $webmention_href_pos+6;
-		$webmention_href_end = @strpos($contents, $quote, $webmention_href_start);
-		$webmention_server_url_len = $webmention_href_end - $webmention_href_start;
-		$webmention_server_url = substr($contents, $webmention_href_start, $webmention_server_url_len);
+  $webmention_link_offset_dquote = strpos($contents, $webmention_str_dquote);
+  $webmention_link_offset_squote = strpos($contents, $webmention_str_squote);
+  if ( $webmention_link_offset_dquote || $webmention_link_offset_squote ) {
+    $quote = ($webmention_link_offset_dquote) ? '"' : '\'';
+    $webmention_link_offset = ($quote=='"') ? $webmention_link_offset_dquote : $webmention_link_offset_squote;
+    $webmention_href_pos = @strpos($contents, 'href=', $webmention_link_offset);
+    $webmention_href_start = $webmention_href_pos+6;
+    $webmention_href_end = @strpos($contents, $quote, $webmention_href_start);
+    $webmention_server_url_len = $webmention_href_end - $webmention_href_start;
+    $webmention_server_url = substr($contents, $webmention_href_start, $webmention_server_url_len);
 
-		// We may find rel="pingback" but an incomplete pingback URL
-		if ( $webmention_server_url_len > 0 ) { // We got it!
-			return $webmention_server_url;
-		}
-	}
+    // We may find rel="pingback" but an incomplete pingback URL
+    if ( $webmention_server_url_len > 0 ) { // We got it!
+      return $webmention_server_url;
+    } 
+  }
 
-	return false;
+  return false;
 }
