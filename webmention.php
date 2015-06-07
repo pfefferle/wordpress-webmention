7336fe1c6a73a81b7f2d0803e4cccedb4abaec8b
Peter Molnar <hello@petermolnar.eu>
2015-06-07 11:28:24 +0100

    replaced link matching regex with http://daringfireball.net/2010/07/improved_regex_for_matching_urls solution to match markdown & inline text urls

--- webmention.php
+++ webmention.php
@@ -408,8 +408,15 @@ class WebMentionPlugin {
     $links = array();
 
     // Find all external links in the source
-    if (preg_match_all("/<a[^>]+href=.(https?:\/\/[^'\"]+)/i", $post->post_content, $matches)) {
-      $links = $matches[1];
+    // http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+    if ( preg_match_all('/(?xi)\b((?:https?:\/\/|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))/', $post->post_content, $matches )) {
+
+        // resolve issues with [url](url) type markdown links
+        foreach ($matches[2] as $cntr => $url )
+            if ( !empty($url) )
+                $matches[1][$cntr] = $url;
+
+        $links = $matches[1];
     }
 
     // filter links
