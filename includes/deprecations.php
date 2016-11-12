26514f9e1542e8f25dda0193676ecd6de34d916d
Matthias Pfefferle <pfefferle@gmail.com>
2016-11-12 23:25:50 +0100

    enable localhost if WP_DEBUG is enabled

--- includes/deprecations.php
+++ includes/deprecations.php
@@ -24,29 +24,29 @@ if (
 	<?php
 	}
 	add_action( 'admin_notices', 'webmention_show_deprecated_filter_warning' );
-}
 
-/**
- * Re-Implement deprecated hooks for a smooth transition
- *
- * @param  array $commentdata the unfiltert comment array
- * @return array              the filtert comment array
- */
-function webmention_deprecated_filters( $commentdata ) {
-	if ( ! $commentdata || is_wp_error( $commentdata ) ) {
+	/**
+	 * Re-Implement deprecated hooks for a smooth transition
+	 *
+	 * @param  array $commentdata the unfiltert comment array
+	 * @return array              the filtert comment array
+	 */
+	function webmention_deprecated_filters( $commentdata ) {
+		if ( ! $commentdata || is_wp_error( $commentdata ) ) {
+			return $commentdata;
+		}
+
+		$contents = $commentdata['comment_content'];
+		$target = $commentdata['target'];
+		$source = $commentdata['comment_author_url'];
+
+		$commentdata['comment_author'] = apply_filters( 'webmention_title', $commentdata['comment_author'], $contents, $target, $source );
+		$commentdata['comment_content'] = apply_filters( 'webmention_content', $commentdata['comment_content'], $contents, $target, $source );
+		$commentdata['comment_type'] = apply_filters( 'webmention_comment_type', $commentdata['comment_type'] );
+		$commentdata['comment_approved'] = apply_filters( 'webmention_comment_approve', $commentdata['comment_approved'] );
+		$commentdata['comment_parent'] = apply_filters( 'webmention_comment_parent', $commentdata['comment_parent'], $target );
+
 		return $commentdata;
 	}
-
-	$contents = $commentdata['comment_content'];
-	$target = $commentdata['target'];
-	$source = $commentdata['comment_author_url'];
-
-	$commentdata['comment_author'] = apply_filters( 'webmention_title', $commentdata['comment_author'], $contents, $target, $source );
-	$commentdata['comment_content'] = apply_filters( 'webmention_content', $commentdata['comment_content'], $contents, $target, $source );
-	$commentdata['comment_type'] = apply_filters( 'webmention_comment_type', $commentdata['comment_type'] );
-	$commentdata['comment_approved'] = apply_filters( 'webmention_comment_approve', $commentdata['comment_approved'] );
-	$commentdata['comment_parent'] = apply_filters( 'webmention_comment_parent', $commentdata['comment_parent'], $target );
-
-	return $commentdata;
+	add_filter( 'webmention_comment_data', 'webmention_deprecated_filters', 31 );
 }
-add_filter( 'webmention_comment_data', 'webmention_deprecated_filters', 31 );
