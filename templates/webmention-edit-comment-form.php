897eb37416390f239d95a930261466a144fb191f
David Shanske <david@shanske.com>
2019-09-07 05:30:58 +0000

    Remove showing the fragment as it is stored for querying not for viewing as part of the target

--- templates/webmention-edit-comment-form.php
+++ templates/webmention-edit-comment-form.php
@@ -3,10 +3,6 @@
 <input type="url" class="widefat" disabled value="<?php echo get_comment_meta( $comment->comment_ID, 'webmention_target_url', true ); ?>" />
 <br />
 
-<label><?php esc_html_e( 'Webmention Target Fragment', 'webmention' ); ?></label>
-<input type="text" class="widefat" disabled value="<?php echo get_comment_meta( $comment->comment_ID, 'webmention_target_fragment', true ); ?>" />
-<br />
-
 <label><?php esc_html_e( 'Webmention Source', 'webmention' ); ?></label>
 <input type="url" class="widefat" disabled value="<?php echo get_comment_meta( $comment->comment_ID, 'webmention_source_url', true ); ?>" />
 <br />
