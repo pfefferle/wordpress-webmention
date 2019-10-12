656854e1c23535f4c9d440ba1391a44c2b6ad7f9
David Shanske <david@shanske.com>
2019-10-12 12:41:10 +0000

    Update readme and version

--- webmention.php
+++ webmention.php
@@ -5,7 +5,7 @@
  * Description: Webmention support for WordPress posts
  * Author: Matthias Pfefferle
  * Author URI: https://notiz.blog/
- * Version: 3.8.11
+ * Version: 4.0.0
  * License: MIT
  * License URI: https://opensource.org/licenses/MIT
  * Text Domain: webmention
