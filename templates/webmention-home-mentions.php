f39bfcb35c0d835d2d7908d8ebe6fd393a329c4a
Michael Beckwith <tw2113@gmail.com>
2019-11-29 09:56:30 -0600

    add some extra classes including unique classes for each webmention listed

--- templates/webmention-home-mentions.php
+++ templates/webmention-home-mentions.php
@@ -1,8 +1,8 @@
-<ul id="webmention-home-mentions-list">
+<ul id="webmention-home-mentions-list" class="webmention-home-mentions-list">
 <?php
 foreach ( $home_mentions as $mention ) {
 	$mention_content = get_comment_text( $mention );
-	echo "<li>{$mention_content} by {$mention->comment_author}</li>";
+	echo "<li class='mention-{$mention->comment_ID}'>{$mention_content} by {$mention->comment_author}</li>";
 }
 ?>
 </ul>
