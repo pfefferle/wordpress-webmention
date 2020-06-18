509acc80716b9b232b2ccf9f7bdb4f547d734ec8
Matthias Pfefferle <pfefferle@gmail.com>
2020-06-18 16:19:51 +0200

    added PHPDoc

--- includes/class-webmention-handler.php
+++ includes/class-webmention-handler.php
@@ -11,10 +11,21 @@ abstract class Webmention_Handler {
 	 */
 	protected $webmention_entity;
 
+	/**
+	 * Get Webmention_Entity
+	 *
+	 * @return Webmention_Entity
+	 */
 	public function get_webmention_entity() {
 		return $this->webmention_entity;
 	}
 
+	/**
+	 * Set Webmention_Entity
+	 *
+	 * @param Webmention_Entity $webmention_entity the Webmention Entity
+	 * @return WP_Error|true
+	 */
 	public function set_webmention_entity( $webmention_entity ) {
 		if ( $webmention_entity instanceof Webmention_Entity ) {
 			return WP_Error( 'wrong_data_format', __( '$webmention_entity is not an instance of Webmention_Entity', 'webmention' ), $webmention_entity );
