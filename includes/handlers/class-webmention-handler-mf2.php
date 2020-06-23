ea9d3154ae4a759fdeb9f3c2ad9ad835dddb5f65
David Shanske <david@shanske.com>
2020-06-23 01:20:30 +0000

    Commit basic class for MF2

---
+++ includes/handlers/class-webmention-handler-mf2.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Class for webmention parsing using Microformats 2.
+*/
+class Webmention_Handler_MF2 extends Webmention_Handler_Base {
+
+	/**
+	 * Takes a request object and parses it.
+	 *
+	 * @param Webmention_Request $request Request Object.
+	 * @param Webmention_Item $item A Parsed Item. If null, a new one will be created.
+	 * @return WP_Error|true Return error or true if successful.
+	 */
+	public function parse( $request, $item = null ) {
+		if ( $item instanceof Webmention_Item ) {
+			$this->webmention_item = $item;
+		}
+		$dom = clone $request->get_domdocument();
+		if ( ! class_exists( '\Webmention\Mf2\Parser' ) ) {
+			require_once plugin_dir_path( __DIR__ ) . 'libraries/mf2/Mf2/Parser.php';
+		}
+		$parser   = new Webmention\Mf2\Parser( $domdocument, $url );
+		$mf_array = $parser->parse();
+
+		// Set raw data.
+		$this->webmention_item->set__raw( $mf_array );
+	}
+
+
+}
