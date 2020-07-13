7fe50bc4fe023365acd65f065f6c5728692b1555
Matthias Pfefferle <pfefferle@gmail.com>
2020-07-13 15:18:37 +0200

    fix indents

--- templates/webmention-tools.php
+++ templates/webmention-tools.php
@@ -1,10 +1,10 @@
 <div class="wrap">
 	<h1><?php esc_html_e( 'Webmention Testing Tools', 'webmention' ); ?></h1>
 
-		<h2> <?php esc_html_e( 'Webmention Parsing Debugger', 'webmention' ); ?> </h2>
-			<form method="get" action="<?php echo esc_url( rest_url( '/webmention/1.0/parse/' ) ); ?> ">
-					<p><label for="url"><?php esc_html_e( 'URL', 'webmention' ); ?></label><input type="url" class="widefat" name="url" id="url" /></p>
-					<?php wp_nonce_field( 'wp_rest' ); ?>
-					<?php submit_button( __( 'Parse', 'webmention' ) ); ?>
-			</form>
+	<h2> <?php esc_html_e( 'Webmention Parsing Debugger', 'webmention' ); ?> </h2>
+	<form method="get" action="<?php echo esc_url( rest_url( '/webmention/1.0/parse/' ) ); ?> ">
+		<p><label for="url"><?php esc_html_e( 'URL', 'webmention' ); ?></label><input type="url" class="widefat" name="url" id="url" /></p>
+		<?php wp_nonce_field( 'wp_rest' ); ?>
+		<?php submit_button( __( 'Parse', 'webmention' ) ); ?>
+	</form>
 </div>
