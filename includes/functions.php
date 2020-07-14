4c4c5d2ad57d1cce1eee6e5e0e427c1a0acd726c
David Shanske <david@shanske.com>
2020-07-14 02:49:59 +0000

    Add ifset function

--- includes/functions.php
+++ includes/functions.php
@@ -586,3 +586,16 @@ if ( ! function_exists( 'normalize_url' ) ) {
 		}
 	}
 }
+
+if ( ! function_exists( 'ifset' ) ) {
+		/**
+		 * If set, return otherwise false.
+		 *
+		 * @param type $var Check if set.
+		 * @return $var|false Return either $var or $return.
+		 */
+	function ifset( &$var, $return = false ) {
+
+			return isset( $var ) ? $var : $return;
+	}
+}
