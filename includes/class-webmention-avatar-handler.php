63361b84e3a8c8017eb48148b550e8044f3c3f23
David Shanske <david@shanske.com>
2021-05-15 19:42:55 +0000

    Remove gravatar caching and the anonymous locally hosted file because you can now cache any of the gravatar defaults and added special casing to save processing time with gravatar

--- includes/class-webmention-avatar-handler.php
+++ includes/class-webmention-avatar-handler.php
@@ -12,15 +12,10 @@ class Webmention_Avatar_Handler {
 		$cls = get_called_class();
 
 		add_filter( 'pre_get_avatar_data', array( $cls, 'avatar_stored_in_comment' ), 30, 2 );
-		add_filter( 'get_avatar_data', array( $cls, 'anonymous_avatar_data' ), 30, 2 );
-
-		// All the default gravatars come from Gravatar instead of being generated locally so add a local default
-		add_filter( 'avatar_defaults', array( $cls, 'anonymous_avatar' ) );
 
 		// Store Avatars Locally
 		add_action( 'comment_post', array( $cls, 'store_avatar' ), 20 );
 		add_action( 'edit_comment', array( $cls, 'store_avatar' ), 20 );
-
 	}
 
 	/**
@@ -46,16 +41,27 @@ class Webmention_Avatar_Handler {
 		$filehandle = '/webmention/avatars/' . $host . '/' . md5( $author ) . '.jpg';
 		$filepath   = $upload_dir['basedir'] . $filehandle;
 
-		// Download Profile Picture and add as attachment
-		$file = wp_get_image_editor( download_url( $url, 300 ) );
-
-		if ( is_wp_error( $file ) ) {
-			$file = wp_get_image_editor( download_url( plugin_dir_url( dirname( __FILE__ ) ) . 'img/mm.jpg', 300 ) );
+		// If this is a gravatar URL, automatically use gravatar to get the right size and file type.
+		if ( strpos( $url, 'gravatar.com' ) ) {
+			$hash = wp_parse_url( $url, PHP_URL_PATH );
+			$hash = str_replace( '/avatar/', '', $hash );
+			$url  = 'https://www.gravatar.com/avatar/' . $hash . '.jpg?s=' . WEBMENTION_AVATAR_SIZE;
+			$file = download_url( $url, 300 );
+			if ( is_wp_error( $file ) {
+				return false;
+			}
+			copy( $file, $filepath );
+			@unlink( $tmp_file );
+		} else {
+			// Download Profile Picture and add as attachment
+			$file = wp_get_image_editor( download_url( $url, 300 ) );
+			if ( is_wp_error( $file ) ) {
+				return false;
+			}
+			$file->resize( null, WEBMENTION_AVATAR_SIZE, true );
+			$file->set_quality( WEBMENTION_AVATAR_QUALITY );
+			$file->save( $filepath, 'image/jpg' );
 		}
-
-		$file->resize( null, WEBMENTION_AVATAR_SIZE, true );
-		$file->set_quality( WEBMENTION_AVATAR_QUALITY );
-		$file->save( $filepath, 'image/jpg' );
 
 		return ( $upload_dir['baseurl'] . '/' . ltrim( $filehandle, '/' ) );
 	}
@@ -134,7 +140,7 @@ class Webmention_Avatar_Handler {
 		$author = get_comment_author_url( $comment );
 		$host   = webmention_get_user_domain( $comment );
 		$avatar = self::sideload_avatar( $avatar, $host, $author );
-
+			
 		delete_comment_meta( $comment->comment_ID, 'semantic_linkbacks_avatar' );
 		update_comment_meta( $comment->comment_ID, 'avatar', $avatar );
 	}
@@ -170,103 +176,6 @@ class Webmention_Avatar_Handler {
 		}
 
 		return $avatar;
-	}
-
-
-	/**
-	 * Function to retrieve default avatar URL
-	 *
-	 *
-	 * @param string $type Default Avatar URL
-	 *
-	 * @return string|boolean $url
-	 */
-	public static function get_default_avatar( $type = null ) {
-		if ( ! $type ) {
-			$type = get_option( 'avatar_default', 'mystery' );
-		}
-
-		switch ( $type ) {
-			case 'mm':
-			case 'mystery':
-			case 'mysteryman':
-				return plugin_dir_url( dirname( __FILE__ ) ) . 'img/mm.jpg';
-		}
-
-		return apply_filters( 'webmention_default_avatar', $type );
-	}
-
-	/**
-	 * Function to check if there is a gravatar
-	 *
-	 *
-	 * @param WP_Comment $comment
-	 *
-	 * @return boolean
-	 */
-	public static function check_gravatar( $comment, $args = null ) {
-		if ( ! empty( $comment->comment_author_email ) ) {
-			$hash = md5( strtolower( trim( $comment->comment_author_email ) ) );
-		} elseif ( is_array( $args ) && array_key_exists( 'url', $args ) ) {
-			if ( ! strpos( $args['url'], 'gravatar.com' ) ) {
-				return false;
-			}
-			$hash = wp_parse_url( $args['url'], PHP_URL_PATH );
-			$hash = str_replace( '/avatar/', '', $hash );
-		} else {
-			return false;
-		}
-
-		$found = get_transient( 'webmention_gravatar_' . $hash );
-
-		if ( false !== $found ) {
-			return $found;
-		} else {
-			$url      = 'https://www.gravatar.com/avatar/' . $hash . '?d=404';
-			$response = wp_remote_head( $url );
-			$found    = ( is_wp_error( $response ) || 404 === wp_remote_retrieve_response_code( $response ) ) ? 0 : 1;
-			set_transient( 'webmention_gravatar_' . $hash, $found, WEBMENTION_GRAVATAR_CACHE_TIME );
-		}
-
-		return $found;
-	}
-
-	/**
-	 * Replaces the default avatar with a locally stored default
-	 *
-	 * @param array      $args    Arguments passed to get_avatar_data(), after processing.
-	 * @param WP_Comment $comment A comment object
-	 *
-	 * @return array $args
-	 */
-	public static function anonymous_avatar_data( $args, $comment ) {
-		if ( ! $comment instanceof WP_Comment ) {
-			return $args;
-		}
-
-		$local = apply_filters( 'webmention_local_avatars', array( 'mm', 'mystery', 'mysteryman' ) );
-
-		if ( ! in_array( $args['default'], $local, true ) ) {
-			return $args;
-		}
-
-		// Always override if default forced
-		if ( $args['force_default'] ) {
-			$args['url'] = self::get_default_avatar( $args['default'] );
-			return $args;
-		}
-
-		if ( ! strpos( $args['url'], 'gravatar.com' ) ) {
-			return $args;
-		}
-
-		if ( self::check_gravatar( $comment, $args ) ) {
-			return $args;
-		}
-
-		$args['url'] = self::get_default_avatar();
-
-		return $args;
 	}
 
 	/**
