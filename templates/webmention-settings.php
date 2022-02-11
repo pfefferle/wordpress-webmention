3d48bdb29ea00b713170cc8b49456c1150c80c8e
Matthias Pfefferle <pfefferle@gmail.com>
2022-02-11 13:19:35 +0100

    remove semantic-linkback reminder

--- templates/webmention-settings.php
+++ templates/webmention-settings.php
@@ -6,12 +6,6 @@
 
 		<h2 class="title"><?php _e( 'Sender', 'webmention' ); ?></h2>
 
-		<?php if ( ! class_exists( 'Semantic_Linkbacks_Plugin' ) ) : ?>
-		<div class="notice notice-warning">
-			<p><?php printf ( __( 'The Webmention plugin primarily handles sending/receiving notifications of mentions from other websites, so the format of the comments can look odd on one\'s site. We highly recommend also installing and activating the <a class="thickbox open-plugin-details-modal" href="%1$s" target_"blank">Semantic Linkbacks Plugin</a> which has better parsing and display capabilities to allow richer looking comments as well as options for displaying many reply types as facepiles for improved user interface.', 'webmention' ), admin_url( '/plugin-install.php?tab=plugin-information&plugin=semantic-linkbacks&TB_iframe=true' ) ); ?></p>
-		</div>
-		<?php endif; ?>
-
 		<p><?php esc_html_e( 'A Webmention Sender is an implementation that sends Webmentions.', 'webmention' ); ?></p>
 
 		<table class="form-table">
