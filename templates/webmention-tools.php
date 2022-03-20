15d01da54a83fa22d42bd0a707a7f2a0c5c260a8
mfeldmann <Marko.Feldmann@ionos.com>
2022-03-20 22:51:19 +0100

    fix for uggly permalinks

--- templates/webmention-tools.php
+++ templates/webmention-tools.php
@@ -4,6 +4,7 @@
 	<h2><?php esc_html_e( 'Webmention Parsing Debugger', 'webmention' ); ?></h2>
 	<p><?php esc_html_e( 'This tests the parsing of an incoming webmention to show what information is available to produce a rich comment', 'webmention' ); ?></p>
 	<form method="get" action="<?php echo esc_url( rest_url( '/webmention/1.0/parse/' ) ); ?> ">
+		<?php echo ! empty( get_option( 'permalink_structure' ) ) ?: '<input type="hidden"  name="rest_route" value="/webmention/1.0/parse/" />' ?>
 		<p><strong><label for="source"><?php esc_html_e( 'Source URL', 'webmention' ); ?></label></strong></p>
 		<input type="url" class="regular-text" name="source" id="source" />
 		<p><strong><label for="target"><?php esc_html_e( 'Target', 'webmention' ); ?></label></strong></p>
