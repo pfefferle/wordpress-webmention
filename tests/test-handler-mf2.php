5bfefba979d0f883dd016f5e1801e1d1ef21dfac
Matthias Pfefferle <pfefferle@gmail.com>
2022-05-24 22:17:14 +0200

    fix tests

--- tests/test-handler-mf2.php
+++ tests/test-handler-mf2.php
@@ -1,4 +1,7 @@
 <?php
+
+use DMS\PHPUnitExtensions\ArraySubset\Assert;
+
 class Webmention_Handler_MF2_Test extends WP_UnitTestCase {
 	/**
 	 * @dataProvider template_provider
@@ -19,12 +22,7 @@ class Webmention_Handler_MF2_Test extends WP_UnitTestCase {
 
 		$subset = json_decode( file_get_contents( substr( $path, 0, -4 ) . 'json' ), true );
 
-		$array = $handler->get_webmention_item()->to_array();
-
-		foreach ( $subset as $key => $value ) {
-			$this->assertArrayHasKey( $key, $array );
-			$this->assertSame( $value, $array[ $key ] );
-		}
+		Assert::assertArraySubset( $subset, $handler->get_webmention_item()->to_array() );
 	}
 
 	public function template_provider() {
