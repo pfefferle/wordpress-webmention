1cda8aa8031148acfbb302b8228cfefef293dee7
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-04-09 16:39:36 +0200

    no need to mention "Webmention"

--- templates/webmention-edit-comment-form.php
+++ templates/webmention-edit-comment-form.php
@@ -1,16 +1,16 @@
 <?php global $comment; ?>
-<label><?php esc_html_e( 'Webmention Target', 'webmention' ); ?></label>
+<label><?php esc_html_e( 'Target', 'webmention' ); ?></label>
 <input type="url" class="widefat" disabled value="<?php echo get_comment_meta( $comment->comment_ID, 'webmention_target_url', true ); ?>" />
 <br />
 
-<label><?php esc_html_e( 'Webmention Source', 'webmention' ); ?></label>
+<label><?php esc_html_e( 'Source', 'webmention' ); ?></label>
 <input type="url" class="widefat" disabled value="<?php echo get_comment_meta( $comment->comment_ID, 'webmention_source_url', true ); ?>" />
 <br />
 
-<label><?php esc_html_e( 'Webmention Creation Time', 'webmention' ); ?></label>
+<label><?php esc_html_e( 'Creation Time', 'webmention' ); ?></label>
 <input type="url" class="widefat" disabled value="<?php echo get_comment_meta( $comment->comment_ID, 'webmention_created_at', true ); ?>" />
 <br />
 
-<label><?php esc_html_e( 'Webmention Response Type', 'webmention' ); ?></label>
+<label><?php esc_html_e( 'Response Type', 'webmention' ); ?></label>
 <input type="url" class="widefat" disabled value="<?php echo $comment->comment_type; ?>" />
 <br />
