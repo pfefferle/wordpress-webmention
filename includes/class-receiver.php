c24e5d292c6c74b6464785b68392a66cd42091b0
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-07-04 11:57:02 +0200

    fix coding style

--- includes/class-receiver.php
+++ includes/class-receiver.php
@@ -567,7 +567,7 @@ class Receiver {
 		$args = array(
 			'post_id'    => $commentdata['comment_post_ID'],
 			'meta_query' => array( $meta_query ),
-			'status' => 'any',
+			'status'     => 'any',
 		);
 
 		if ( ! empty( $fragment ) ) {
