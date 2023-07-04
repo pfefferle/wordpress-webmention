809b37671872ddb02cba4bcb8b307bb67ecb5abf
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2023-07-04 11:56:21 +0200

    add options to tools page

--- templates/webmention-tools.php
+++ templates/webmention-tools.php
@@ -14,6 +14,12 @@
 		<p><strong><label for="target"><?php esc_html_e( 'Target', 'webmention' ); ?></label></strong></p>
 		<input type="url" class="regular-text" name="target" id="target" />
 
+		<p><strong><label for="source"><?php esc_html_e( 'Output format', 'webmention' ); ?></label></strong></p>
+		<label for="aggregated"><?php esc_html_e( 'Aggregated', 'webmention' ); ?></label>
+		<input type="radio" id="aggregated" name="mode" value="aggregated" checked="checked">
+		<label for="grouped">Grouped</label>
+		<input type="radio" id="grouped" name="mode" value="grouped">
+
 		<p><?php submit_button( __( 'Parse', 'webmention' ), 'small', 'submit', false ); ?></p>
 		<?php wp_nonce_field( 'wp_rest' ); ?>
 	</form>
