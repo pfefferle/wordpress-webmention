75c2c3959a2c769cce3f1b6da73280b67c10e28a
Matthias Pfefferle <pfefferle@users.noreply.github.com>
2025-12-11 19:06:12 +0100

    Refactor test infrastructure to match ActivityPub plugin
    - Update composer.json with modern dev dependencies
    - Update phpcs.xml with parallel processing and VariableAnalysis
    - Update phpunit.xml.dist for PHPUnit 10 compatibility
    - Reorganize tests to tests/phpunit/ structure with class-test- prefix
    - Update GitHub Actions workflows with path filters and latest actions
    - Replace grunt with wp-env for local development
    - Add .wp-env.json configuration

--- tests/phpunit/tests/class-test-discovery.php
+++ tests/phpunit/tests/class-test-discovery.php
@@ -1,36 +1,74 @@
 <?php
-class Discovery_Test extends WP_UnitTestCase {
+/**
+ * Test webmention discovery.
+ *
+ * @package Webmention
+ */
 
-	public function headers( $link = "") {
+/**
+ * Test webmention discovery class.
+ */
+class Test_Discovery extends WP_UnitTestCase {
+
+	/**
+	 * Get mock headers.
+	 *
+	 * @param string $link Link header value.
+	 * @return array Headers array.
+	 */
+	public function headers( $link = '' ) {
 		$headers = array(
-			'server' => 'nginx/1.9.15',
-			'date' => 'Mon, 16 May 2016 01:21:08 GMT',
+			'server'       => 'nginx/1.9.15',
+			'date'         => 'Mon, 16 May 2016 01:21:08 GMT',
 			'content-type' => 'text/html; charset=UTF-8',
-			'link' => $link,
+			'link'         => $link,
 		);
 		return $headers;
 	}
 
-	public function response( $code = 200, $response = "OK" ) {
+	/**
+	 * Get mock response.
+	 *
+	 * @param int    $code     Response code.
+	 * @param string $response Response message.
+	 * @return array Response array.
+	 */
+	public function response( $code = 200, $response = 'OK' ) {
 		$response = array(
-			'code' => $code,
+			'code'     => $code,
 			'response' => $response,
 		);
 		return $response;
 	}
 
+	/**
+	 * Get mock HTTP return.
+	 *
+	 * @param array  $headers  Headers.
+	 * @param array  $response Response.
+	 * @param string $body     Body.
+	 * @return array HTTP return array.
+	 */
 	public function httpreturn( $headers, $response, $body ) {
 		return compact( 'headers', 'response', 'body' );
 	}
 
+	/**
+	 * Mock discover relative HTTP link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_relative_httplink() {
-		$link = '</test/1/webmention>; rel=webmention';
-		$headers = $this->headers( $link );
+		$link     = '</test/1/webmention>; rel=webmention';
+		$headers  = $this->headers( $link );
 		$response = $this->response( 200, 'OK' );
-		$body = '<html></html>';
+		$body     = '<html></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover relative HTTP link.
+	 */
 	public function test_discover_relative_httplink() {
 		$url = 'http://www.example.com/test/1';
 
@@ -39,14 +77,22 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/1/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover absolute HTTP link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_absolute_httplink() {
-		$link = '<http://www.example.com/test/2/webmention>; rel=webmention';
-		$headers = $this->headers( $link );
+		$link     = '<http://www.example.com/test/2/webmention>; rel=webmention';
+		$headers  = $this->headers( $link );
 		$response = $this->response( 200, 'OK' );
-		$body = '<html></html>';
+		$body     = '<html></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover absolute HTTP link.
+	 */
 	public function test_discover_absolute_httplink() {
 		$url = 'http://www.example.com/test/2';
 
@@ -55,14 +101,22 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/2/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover quoted HTTP link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_quoted_httplink() {
-		$link = '<http://www.example.com/test/8/webmention>; rel="webmention"';
-		$headers = $this->headers( $link );
+		$link     = '<http://www.example.com/test/8/webmention>; rel="webmention"';
+		$headers  = $this->headers( $link );
 		$response = $this->response( 200, 'OK' );
-		$body = '<html></html>';
+		$body     = '<html></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover quoted HTTP link.
+	 */
 	public function test_discover_quoted_httplink() {
 		$url = 'http://www.example.com/test/2';
 
@@ -71,14 +125,22 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/8/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover multiple HTTP link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_multiple_httplink() {
-		$link = '<http://www.example.com/test/10/webmention>; rel="webmention somethingelse"';
-		$headers = $this->headers( $link );
+		$link     = '<http://www.example.com/test/10/webmention>; rel="webmention somethingelse"';
+		$headers  = $this->headers( $link );
 		$response = $this->response( 200, 'OK' );
-		$body = '<html></html>';
+		$body     = '<html></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover multiple HTTP link.
+	 */
 	public function test_discover_multiple_httplink() {
 		$url = 'http://www.example.com/test/10';
 
@@ -87,16 +149,24 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/10/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover case sensitive HTTP link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_casesensitive_httplink() {
-		$link = '<http://www.example.com/test/7/webmention>; rel=webmention';
-		$headers = $this->headers( $link );
-		$headers['Link'] = $headers['link'];
+		$link                = '<http://www.example.com/test/7/webmention>; rel=webmention';
+		$headers             = $this->headers( $link );
+		$headers['Link']     = $headers['link'];
 		unset( $headers['link'] );
 		$response = $this->response( 200, 'OK' );
-		$body = '<html></html>';
+		$body     = '<html></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover case sensitive HTTP link.
+	 */
 	public function test_discover_casesensitive_httplink() {
 		$url = 'http://www.example.com/test/7';
 
@@ -105,13 +175,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertFalse( $endpoint );
 	}
 
+	/**
+	 * Mock discover relative HTML link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_relative_htmllink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html>\r\n<html lang="en"><head><link rel="webmention" href="/test/3/webmention"></head><body>This is a test.</body></html>';
+		$body     = '<!DOCTYPE html>\r\n<html lang="en"><head><link rel="webmention" href="/test/3/webmention"></head><body>This is a test.</body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover relative HTML link.
+	 */
 	public function test_discover_relative_htmllink() {
 		$url = 'http://www.example.com/test/3';
 
@@ -120,13 +198,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/3/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover absolute HTML link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_absolute_htmllink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head><link rel="webmention" href="http://www.example.com/test/4/webmention"></head><body>This is a test</body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head><link rel="webmention" href="http://www.example.com/test/4/webmention"></head><body>This is a test</body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover absolute HTML link.
+	 */
 	public function test_discover_absolute_htmllink() {
 		$url = 'http://www.example.com/test/4';
 
@@ -135,13 +221,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/4/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover multiple HTML link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_multiple_htmllink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head><link rel="webmention somethingelse" href="http://www.example.com/test/9/webmention"></head><body>This is a test</body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head><link rel="webmention somethingelse" href="http://www.example.com/test/9/webmention"></head><body>This is a test</body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover multiple HTML link.
+	 */
 	public function test_discover_multiple_htmllink() {
 		$url = 'http://www.example.com/test/9';
 
@@ -151,13 +245,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/9/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover not webmention HTML link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_notwebmention_htmllink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head><link rel="not-webmention" href="http://www.example.com/test/12/webmention"></head><body>This is a testThere is also a <a href="/test/121/webmention" rel="webmention"></body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head><link rel="not-webmention" href="http://www.example.com/test/12/webmention"></head><body>This is a testThere is also a <a href="/test/121/webmention" rel="webmention"></body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover not webmention HTML link.
+	 */
 	public function test_discover_notwebmention_htmllink() {
 		$url = 'http://www.example.com/test/12';
 
@@ -166,13 +268,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/121/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover empty HTML link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_empty_htmllink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head><link rel="webmention" href=""></head><body>This is a test</body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head><link rel="webmention" href=""></head><body>This is a test</body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover empty HTML link.
+	 */
 	public function test_discover_empty_htmllink() {
 		$url = 'http://www.example.com/test/15';
 
@@ -181,13 +291,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/15', $endpoint );
 	}
 
+	/**
+	 * Mock discover relative href link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_relative_hreflink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head></head><body><a rel="webmention" href="/test/5/webmention">Webmention</a></body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head></head><body><a rel="webmention" href="/test/5/webmention">Webmention</a></body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover relative href link.
+	 */
 	public function test_discover_relative_hreflink() {
 		$url = 'http://www.example.com/test/5';
 
@@ -196,13 +314,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/5/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover absolute href link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_absolute_hreflink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head></head><body><a rel="webmention" href="http://www.example.com/test/6/webmention">Webmention</a></body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head></head><body><a rel="webmention" href="http://www.example.com/test/6/webmention">Webmention</a></body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover absolute href link.
+	 */
 	public function test_discover_absolute_hreflink() {
 		$url = 'http://www.example.com/test/6';
 
@@ -211,13 +337,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/6/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover comment href link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_comment_hreflink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head></head><body><!-- <a rel="webmention" href="http://www.example.com/test/13/webmention">Webmention</a> --><a rel="webmention" href="http://www.example.com/test/131/webmention">Webmention</a></body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head></head><body><!-- <a rel="webmention" href="http://www.example.com/test/13/webmention">Webmention</a> --><a rel="webmention" href="http://www.example.com/test/131/webmention">Webmention</a></body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover comment href link.
+	 */
 	public function test_discover_comment_hreflink() {
 		$url = 'http://www.example.com/test/13';
 
@@ -226,13 +360,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/131/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover escaped href link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_escaped_hreflink() {
-		$headers = $this->headers( '' );
+		$headers  = $this->headers( '' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head></head><body><code>&lt;a rel="webmention" href="http://www.example.com/test/14/webmention">Webmention&gt;&lt;/a&gt;<a rel="webmention" href="http://www.example.com/test/141/webmention">Webmention</a></body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head></head><body><code>&lt;a rel="webmention" href="http://www.example.com/test/14/webmention">Webmention&gt;&lt;/a&gt;<a rel="webmention" href="http://www.example.com/test/141/webmention">Webmention</a></body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover escaped href link.
+	 */
 	public function test_discover_escaped_hreflink() {
 		$url = 'http://www.example.com/test/14';
 
@@ -241,13 +383,21 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/141/webmention', $endpoint );
 	}
 
+	/**
+	 * Mock discover multiple link.
+	 *
+	 * @return array HTTP return.
+	 */
 	public function discover_multiple_link() {
-		$headers = $this->headers( '<http://www.example.com/test/11/webmention>; rel=webmention' );
+		$headers  = $this->headers( '<http://www.example.com/test/11/webmention>; rel=webmention' );
 		$response = $this->response( 200, 'OK' );
-		$body = '<!DOCTYPE html><html lang="en"><head><link rel="webmention" href="http://www.example.com/test/4/webmention"></head><body><a rel="webmention" href="http://www.example.com/test/6/webmention">Webmention</a></body></html>';
+		$body     = '<!DOCTYPE html><html lang="en"><head><link rel="webmention" href="http://www.example.com/test/4/webmention"></head><body><a rel="webmention" href="http://www.example.com/test/6/webmention">Webmention</a></body></html>';
 		return $this->httpreturn( $headers, $response, $body );
 	}
 
+	/**
+	 * Test discover multiple link.
+	 */
 	public function test_discover_multiple_link() {
 		$url = 'http://www.example.com/test/11';
 
@@ -256,6 +406,9 @@ class Discovery_Test extends WP_UnitTestCase {
 		$this->assertSame( 'http://www.example.com/test/11/webmention', $endpoint );
 	}
 
+	/**
+	 * Test webmention extract URLs.
+	 */
 	public function test_webmention_extract_urls() {
 		$urls = webmention_extract_urls( '<main><div><a href="https://example.org">Test</a></div><a href="https://example.com">Test</a></main>' );
 
@@ -263,7 +416,7 @@ class Discovery_Test extends WP_UnitTestCase {
 			$urls,
 			array(
 				'https://example.org',
-				'https://example.com'
+				'https://example.com',
 			)
 		);
 	}
