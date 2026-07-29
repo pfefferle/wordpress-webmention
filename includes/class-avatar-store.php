afadf8dba5de216fdf36ddc999a8bb7f76229e04
Terence Eden <edent@users.noreply.github.com>
2026-07-29 14:22:20 +0100

    Check if dir exists
    Fixes #622

--- includes/class-avatar-store.php
+++ includes/class-avatar-store.php
@@ -42,6 +42,9 @@ class Avatar_Store {
 	public static function find_avatar( $host, $author ) {
 		$upload_dir = trailingslashit( self::upload_directory( $host ) );
 		$upload_url = trailingslashit( self::upload_directory( $host, true ) );
+		if ( ! is_dir( $upload_dir ) ) {
+			return false;
+		}
 		$results    = scandir( $upload_dir );
 		if ( ! $results ) {
 			return $results;
